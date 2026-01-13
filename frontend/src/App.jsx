0be713dbb7cf5214d5fc1c8d91e279133527e325
AayushAdhikari30 <aayush.adhikari.9464@gmail.com>
2026-01-13 21:37:29 +0545

    Final Integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import { Toaster } from "react-hot-toast";
 import Login from "./Pages/Login";
 import AdminDashboard from "./Pages/AdminDashboard";
 import StudentDashboard from "./Pages/StudentDashboard";
+import ProfileSettings from "./Pages/ProfileSettings";
+import AccountSettings from "./Pages/AccountSettings";
+import HelpSupport from "./Pages/HelpSupport";
 import Authcontext from './context/authcontext';
 import ProtectedRoute from './componets/common/ProtectedRoute';
 
@@ -31,6 +34,11 @@ function App() {
             <Route path="/login" element={<Login />}/>
             <Route path="/register" element={<Register/>}/>
             <Route path="new-book" element={<ProtectedRoute requiredRole="admin"><NewBook /></ProtectedRoute>} />
+
+            {/* Profile Routes */}
+            <Route path="/profile-settings" element={<ProtectedRoute><ProfileSettings /></ProtectedRoute>} />
+            <Route path="/account-settings" element={<ProtectedRoute><AccountSettings /></ProtectedRoute>} />
+            <Route path="/help-support" element={<ProtectedRoute><HelpSupport /></ProtectedRoute>} />
 
             <Route path="/admin-dashboard" element={<ProtectedRoute requiredRole="admin"><AdminDashboard /></ProtectedRoute>}>
               <Route index element={<Cards />} />
